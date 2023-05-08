1b4419f89b14ecfe43773e8850887b457719ab81
aryan <basam.aryan@gmail.com>
2023-05-07 20:44:43 -0400

    rmx3371: Use low gamma implementation for brightness curve
    Change-Id: I976fc4fdf9c3baae089cb4aa1a64927deb13f87b

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.brightness.low.gamma=true
+    
 # Device init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
