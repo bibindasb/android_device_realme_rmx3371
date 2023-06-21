f69dc7288313cf7f5524912034f307a8bf064f47
Chenyang Zhong <zhongcy95@gmail.com>
2023-06-21 21:22:22 +0900

    rmx3371: displayconfig: Add brightness ramp rate
    Now that Android 12 allows the brightness ramp rate to be changed separately
    for fast/slow and increase/decrease in float. Once loaded,
    DisplayDeviceConfig has higher priority than the integer config
    values in frameworks. The format definition is in [1].
    
    [1]
    frameworks/base/services/core/xsd/display-device-config/display-device-config.xsd
    
    Change-Id: I77d1640316688a4db780d1b3fb64acc5d21714cc

--- device.mk
+++ device.mk
@@ -42,6 +42,9 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_4630946728207028354.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946728207028354.xml
 
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.brightness.low.gamma=true
