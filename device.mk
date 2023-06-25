f1c7610d316bc76ea209d661ee6fa5f684ffba08
bibindasb <bibindasappu1@gmail.com>
2023-06-25 09:59:39 +0000

    RMX3371: audio experiment

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio
+TARGET_HAS_AUDIO_LVIMFS := true
+
+# Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
